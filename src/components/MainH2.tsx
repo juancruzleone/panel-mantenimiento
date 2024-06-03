d8aa46f904899f15312dd8b98c21c806da6288e3
Juan cruz Leone <juancruzleone@outlook.com>
2024-06-03 02:32:59 -0300

    Se modifico la seccion edificios y se agrego un input para buscador

--- src/components/MainH2.tsx
+++ src/components/MainH2.tsx
@@ -2,7 +2,7 @@ type MainH2Props = {
     title: string;
 };
 
-const MainH2 = ({ title }: MainH1Props) => {
+const MainH2 = ({ title }: MainH2Props) => {
     return (
         <>
             <h1 className="font-montserrat font-bold text-3xl text-center pt-24 pl-24">{title}</h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,18 @@ import Link from 'next/link';
 const Index = () => {
   return (
       <>
-      <section className='flex'>
+      <main className='flex'>
         <NavPanel />
         <div>
           <MainH1 title="Edificios"/>
-          <div className='mt-5'>
-            <Link href="/" className="bg-orange-600 p-2 text-white font-montserrat text-center rounded-lg ml-24">Crear</Link>
-          </div>        
+          <div className='mt-7'>
+            <Link href="/" className="bg-boton p-2 text-white font-montserrat text-center rounded-lg ml-[120px]">Crear</Link>
+          </div>
+          <div className='pl-[120px] mt-10'>
+            <input type="text" className='p-3 rounded-full text-black font-montserrat' placeholder='Búsca por titulo'/>     
+          </div>  
         </div>
-      </section>
-      
+      </main>
       </>
   );
 }
