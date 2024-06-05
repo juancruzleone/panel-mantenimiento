7316c2f593468b010f9383cab37e7f24e8fa3d84
Juan cruz Leone <juancruzleone@outlook.com>
2024-06-05 18:46:13 -0300

    Se crearon las rutas para las instalaciones

--- back/api/routes/route.api.instalaciones.ts
+++ back/api/routes/route.api.instalaciones.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import * as controllers from '../controllers/controller.api.instalaciones';
 
+const router = Router();
 
-const route = Router()
+router.get('/instalaciones', controllers.getAllInstalaciones);
+router.get('/instalaciones/:id', controllers.getInstalacionById);
+router.post('/instalaciones', controllers.createInstalacion);
+router.put('/instalaciones/:id', controllers.updateInstalacion);
+router.delete('/instalaciones/:id', controllers.deleteInstalacion);
 
-
-
-export default route
+export default router;
